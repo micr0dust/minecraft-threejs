cad8b2892f93c52337b2824f8875f96bceacd8b4
micr0dust <wuilliamolgo104286@gmail.com>
2022-06-24 14:21:06 +0800

    perlin-noise

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import * as THREE from 'three';
 import { GLTFLoader } from 'https://unpkg.com/three@0.141.0/examples/jsm/loaders/GLTFLoader.js';
 import { PointerLockControls } from 'https://unpkg.com/three@0.141.0/examples/jsm/controls/PointerLockControls.js';
 
@@ -6,8 +7,6 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.01, 100);
 camera.position.set(-15, 2, 0);
 camera.lookAt(-14, 2, 0);
-let walkDirection = new THREE.Vector3(0, 0, 0);
-let speed = 0.1;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -15,61 +14,57 @@ document.body.appendChild(renderer.domElement);
 
 /* PointerLockControls setting */
 const controls = new PointerLockControls(camera, document.body);
-controls.getObject().position.set(-15, 2, 0)
+controls.getObject().position.set(-15, 2, 0);
 controls.getObject().lookAt(-14, 2, 0);
-scene.add(controls.getObject())
+scene.add(controls.getObject());
 
-// add event listener to show/hide a UI (e.g. the game's menu)
-controls.addEventListener('lock', function() {
-    //menu.style.display = 'none';
-    console.log('lock');
-});
+// vars
+let speed = 0.2;
+let blockScale = 1;
 
-controls.addEventListener('unlock', function() {
-    //menu.style.display = 'block';
-    console.log('unlock');
-});
+function grid(range) {
+    for (let i = -range; i < range; i++) {
+        const material_line = new THREE.LineBasicMaterial({ color: 0x0000ff });
+        const points = [];
+        points.push(new THREE.Vector3(i, 0, -range));
+        points.push(new THREE.Vector3(i, 0, range));
+        const geometry_line = new THREE.BufferGeometry().setFromPoints(points);
+        const line = new THREE.Line(geometry_line, material_line);
+        scene.add(line);
+    }
 
-/* cube */
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-
-/* grid start */
-for (let i = -1000; i < 1000; i++) {
-    const material_line = new THREE.LineBasicMaterial({ color: 0x0000ff });
-    const points = [];
-    points.push(new THREE.Vector3(i, 0, -1000));
-    points.push(new THREE.Vector3(i, 0, 1000));
-    const geometry_line = new THREE.BufferGeometry().setFromPoints(points);
-    const line = new THREE.Line(geometry_line, material_line);
-    scene.add(line);
+    for (let i = -range; i < range; i++) {
+        const material_line = new THREE.LineBasicMaterial({ color: 0x0000ff });
+        const points = [];
+        points.push(new THREE.Vector3(-range, 0, i));
+        points.push(new THREE.Vector3(range, 0, i));
+        const geometry_line = new THREE.BufferGeometry().setFromPoints(points);
+        const line = new THREE.Line(geometry_line, material_line);
+        scene.add(line);
+    }
 }
+grid(1000);
 
-for (let i = -1000; i < 1000; i++) {
-    const material_line = new THREE.LineBasicMaterial({ color: 0x0000ff });
-    const points = [];
-    points.push(new THREE.Vector3(-1000, 0, i));
-    points.push(new THREE.Vector3(1000, 0, i));
-    const geometry_line = new THREE.BufferGeometry().setFromPoints(points);
-    const line = new THREE.Line(geometry_line, material_line);
-    scene.add(line);
+function terrain(range) {
+    let blocks = [];
+    let xoff = 0;
+    let yoff = 0;
+    let inc = Math.random() * 0.05;
+    let amplitude = Math.random() * 100;
+    for (let x = 0; x < range; x++) {
+        xoff = 0;
+        for (let z = 0; z < range; z++) {
+            let v = Math.round(noise.perlin2(xoff, yoff) * amplitude / blockScale) * blockScale;
+            blocks.push(new Block(x * blockScale, v, z * blockScale));
+            xoff += inc;
+        }
+        yoff += inc;
+    }
+    for (let i = 0; i < blocks.length; i++) {
+        blocks[i].display();
+    }
 }
-/* grid end */
-
-const image = new Image();
-image.onload = function(img) {
-    const canvas = document.createElement('canvas');
-    const ctx = canvas.getContext('2d');
-    canvas.width = 512;
-    canvas.height = 512;
-    ctx.drawImage(this, 0, 0, 512, 512);
-    material.map = new THREE.Texture(canvas);
-    material.map.needsUpdate = true;
-    scene.add(cube);
-}
-image.crossOrigin = "anonymous";
-image.src = './textures/age_of_gunpowder.png';
+terrain(20);
 
 let key = {};
 window.onload = function() {
@@ -82,8 +77,6 @@ window.onload = function() {
         event.preventDefault();
         key[event.key.toLowerCase()] = event.type == 'keydown';
         //console.log(event.key);
-        //console.log(controls.getObject().position);
-        console.log(controls.getObject().position.y);
     }
 
     onmousemove = function(e) {};
@@ -91,13 +84,43 @@ window.onload = function() {
     window.addEventListener('resize', function() {
         renderer.setSize(window.innerWidth, window.innerHeight);
     });
+
+    // add event listener to show/hide a UI (e.g. the game's menu)
+    controls.addEventListener('lock', function() {
+        //menu.style.display = 'none';
+        console.log('lock');
+    });
+
+    controls.addEventListener('unlock', function() {
+        //menu.style.display = 'block';
+        console.log('unlock');
+    });
+}
+
+function Block(x, y, z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.display = function() {
+        let blockBox = new THREE.BoxBufferGeometry(blockScale, blockScale, blockScale);
+        let blockMesh = new THREE.MeshBasicMaterial({ color: 0x44BC23 });
+        let block = new THREE.Mesh(blockBox, blockMesh);
+        scene.add(block);
+        block.position.x = this.x;
+        block.position.y = this.y;
+        block.position.z = this.z;
+        let edges = new THREE.EdgesGeometry(blockBox);
+        let line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({ color: 0x000000 }));
+        scene.add(line);
+        line.position.x = this.x;
+        line.position.y = this.y;
+        line.position.z = this.z;
+    }
 }
 
 function animate() {
     requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    //camera.lookAt(0, 0, 0);
+
     if (controls.isLocked === true) {
         let lastY = controls.getObject().position.y
         let playerDirection = new THREE.Vector3(0, 0, 0);
@@ -118,6 +141,7 @@ function animate() {
         else controls.getObject().position.y = lastY;
     } else if (key["f11"]) controls.lock();
     document.getElementById('info').innerText = `X:${camera.position.x.toFixed(2)} Y:${camera.position.y.toFixed(2)} Z:${camera.position.z.toFixed(2)}`
+
     renderer.render(scene, camera);
 }
 animate();
